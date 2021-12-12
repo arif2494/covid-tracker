4101232b0705943e08c078e98c440c7cc2892cfd
Ariful Islam <arifulislam161024@gmail.com>
2021-12-12 21:00:43 +0600

    footer added

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,15 @@
 const Footer = () => {
   return (
-    <div>
-      <h1>THis is footer</h1>
+    <div className='bg-black text-white py-4 flex justify-center mt-4'>
+      <h1>
+        Api from
+        <a
+          className='underline decoration-green-500 ml-1'
+          href='https://covidtracking.com/'
+        >
+          covidtracking.com
+        </a>
+      </h1>
     </div>
   );
 };
