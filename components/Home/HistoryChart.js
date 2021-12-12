bc9d6c90b5081f04256b1ff6307181acd4d9b9b7
Ariful Islam <arifulislam161024@gmail.com>
2021-12-12 16:22:20 +0600

    home page graph done

--- components/Home/HistoryChart.js
+++ components/Home/HistoryChart.js
@@ -1,147 +1,20 @@
 import {
-  Area,
-  AreaChart,
   Bar,
   BarChart,
   Brush,
   CartesianGrid,
-  Line,
-  LineChart,
   ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
 } from "recharts";
 
-// const data = [
-//   {
-//     name: "Page A",
-//     uv: 4000,
-//     pv: 2400,
-//     amt: 2400,
-//   },
-//   {
-//     name: "Page B",
-//     uv: 3000,
-//     pv: 1398,
-//     amt: 2210,
-//   },
-//   {
-//     name: "Page C",
-//     uv: 2000,
-//     pv: 9800,
-//     amt: 2290,
-//   },
-//   {
-//     name: "Page D",
-//     uv: 2780,
-//     pv: 3908,
-//     amt: 2000,
-//   },
-//   {
-//     name: "Page E",
-//     uv: 1890,
-//     pv: 4800,
-//     amt: 2181,
-//   },
-//   {
-//     name: "Page F",
-//     uv: 2390,
-//     pv: 3800,
-//     amt: 2500,
-//   },
-//   {
-//     name: "Page G",
-//     uv: 3490,
-//     pv: 4300,
-//     amt: 2100,
-//   },
-// ];
-
 const HistoryChart = ({ data }) => {
   return (
     <div style={{ width: "100%" }}>
-      <h4>A demo of synchronized AreaCharts</h4>
-
-      <ResponsiveContainer width='100%' height={200}>
-        <LineChart
-          width={500}
-          height={200}
-          data={data}
-          syncId='anyId'
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray='3 3' />
-          <XAxis dataKey='date' />
-          <YAxis />
-          <Tooltip />
-          <Line
-            type='monotone'
-            dataKey='positive'
-            stroke='#8884d8'
-            fill='#8884d8'
-          />
-        </LineChart>
-      </ResponsiveContainer>
-      <p>Maybe some other content</p>
-
-      <ResponsiveContainer width='100%' height={200}>
-        <LineChart
-          width={500}
-          height={200}
-          data={data}
-          syncId='anyId'
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray='3 3' />
-          <XAxis dataKey='date' />
-          <YAxis />
-          <Tooltip />
-          <Line
-            type='monotone'
-            dataKey='negative'
-            stroke='#82ca9d'
-            fill='#82ca9d'
-          />
-        </LineChart>
-      </ResponsiveContainer>
-
-      <ResponsiveContainer width='100%' height={200}>
-        <AreaChart
-          width={500}
-          height={200}
-          data={data}
-          syncId='anyId'
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray='3 3' />
-          <XAxis dataKey='date' />
-          <YAxis />
-          <Tooltip />
-          <Area
-            type='monotone'
-            dataKey='death'
-            stroke='#82ca9d'
-            fill='#82ca9d'
-          />
-          <Brush />
-        </AreaChart>
-      </ResponsiveContainer>
+      <h1 className='text-center font-semibold my-2 text-xl underline decoration-indigo-600'>
+        Statistics Report of Covid-19 USA
+      </h1>
       <ResponsiveContainer width='100%' height={200}>
         <BarChart
           width={150}
@@ -160,9 +33,86 @@ const HistoryChart = ({ data }) => {
           <YAxis />
           <Tooltip />
 
-          <Bar dataKey='pending' fill='#8884d8' />
+          <Bar dataKey='positive' fill='#dc2626' />
         </BarChart>
       </ResponsiveContainer>
+      <h4 className='my-2 text-center text-red-600'>
+        People Affected by Covid:19 In USA
+      </h4>
+
+      <ResponsiveContainer width='100%' height={200}>
+        <BarChart
+          width={150}
+          height={40}
+          data={data}
+          syncId='anyId'
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='date' />
+          <YAxis />
+          <Tooltip />
+
+          <Bar dataKey='negative' fill='#16a34a' />
+        </BarChart>
+      </ResponsiveContainer>
+      <h4 className='my-2 text-center text-green-600'>
+        People Recovered from Covid:19 In USA
+      </h4>
+      <ResponsiveContainer width='100%' height={200}>
+        <BarChart
+          width={150}
+          height={40}
+          data={data}
+          syncId='anyId'
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='date' />
+          <YAxis />
+          <Tooltip />
+
+          <Bar dataKey='pending' fill='#0284c7' />
+        </BarChart>
+      </ResponsiveContainer>
+      <h4 className='my-2 text-center text-sky-600'>
+        People now fighting with from Covid:19 In USA
+      </h4>
+      <ResponsiveContainer width='100%' height={230}>
+        <BarChart
+          width={150}
+          height={40}
+          data={data}
+          syncId='anyId'
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='date' />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey='death' fill='#d97706' />
+          <Brush />
+        </BarChart>
+      </ResponsiveContainer>
+      <h4 className='my-2 text-center text-amber-600'>
+        People died from Covid:19 In USA
+      </h4>
+      <Brush />
     </div>
   );
 };
